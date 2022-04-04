29bce9a552c4369d79a2d05641a10146e6a993d6
German G Sayago <german.sayago@gmail.com>
2022-04-04 16:56:51 -0300

    was added GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GTM_ID } from '../lib/gtm';
 
 export default function Document() {
   return (
@@ -17,6 +18,14 @@ export default function Document() {
         />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
