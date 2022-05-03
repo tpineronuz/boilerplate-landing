c7805565c8a0f5bd87564bd87ee6a222c06c18c6
German G Sayago <german.sayago@gmail.com>
2022-05-03 16:01:15 -0300

    added new logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Logotipo from '@assets/images/loghitec-logo.svg';
+import Logotipo from '@assets/images/icytec-logo.svg';
 
 export const Logo = () => {
   return (
